1b3575c2a3d8aca25d288d5c48e2de3925f6aeef
KaleviOfficial <t9roaa00@students.oamk.fi>
2025-02-06 18:13:36 +0200

    cv website qt edition

---
+++ cv_website_qt_edition/script2.js
@@ -0,0 +1,5 @@
+document.getElementById('learn-more-btn').addEventListener('click', function(event) {
+    // Store the current scroll position in sessionStorage
+    sessionStorage.setItem('scrollPosition', window.scrollY);
+});
+
